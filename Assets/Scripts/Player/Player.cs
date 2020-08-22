928ede864f4203c2b7e755b20ed6d9398013e3e0
Mark Johnson <Jam103114@gmail.com>
2020-08-22 16:37:46 -0500

    Creating the Enemy Base Class

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BallMotor))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] int _maxHealth = 3;
+    int _currentHealth;
+
+    BallMotor _ballMotor;
+
+    private void Awake()
+    {
+        _ballMotor = GetComponent<BallMotor>();
+    }
+
+    private void Start()
+    {
+        _ballMotor = GetComponent<BallMotor>();
+    }
+
+    private void FixedUpdate()
+    {
+        ProcessMovement();  
+    }
+
+    private void ProcessMovement()
+    {
+        //TODO move into Input script
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical).normalized;
+
+        _ballMotor.Move(movement);
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        Debug.Log("Player's Health: " + _currentHealth);
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        _currentHealth -= amount;
+        Debug.Log("Player's health: " + _currentHealth);
+        if (_currentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Kill()
+    {
+        gameObject.SetActive(false);
+        //do stuff
+    }
+}
